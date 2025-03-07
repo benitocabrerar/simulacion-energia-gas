d8ce0b17ed9151a10f1d74f0633731d952d18443
benitocabrerar <130802254+benitocabrerar@users.noreply.github.com>
2025-03-07 11:30:19 -0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import SimulacionEnergiaGas from './SimulacionEnergiaGas';
+
+function App() {
+  return (
+    <div className="App">
+      <SimulacionEnergiaGas />
+    </div>
+  );
+}
+
+export default App;
